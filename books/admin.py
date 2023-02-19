0765dfae8d6c8abcce2033ffe1a0dd8fc18c4414
andrei <andrei.maftei28@gmail.com>
2023-02-19 12:27:41 +0100

    add searchfield to admin pannel

--- books/admin.py
+++ books/admin.py
@@ -15,6 +15,7 @@ class CustomAddressAdmin(admin.ModelAdmin):
 @admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ("name", "address", "view_same_author_link")
+    search_fields = ("name__startswith", )
     
     def view_same_author_link(self, obj):
         # TODO: fix filter by author
@@ -24,8 +25,9 @@ class AuthorAdmin(admin.ModelAdmin):
         url = (
             reverse("admin:books_book_changelist")
             + "?"
-            + urlencode({"book__id": f"{obj.id}"})
+            + urlencode({"book_author": f"{obj.name.replace(' ', '%20')}"})
         )
+        print(url)
         return format_html('<a href="{}">{} Books</a>', url, count["name__count"])
 
     view_same_author_link.short_description = "Books"
@@ -34,15 +36,19 @@ class AuthorAdmin(admin.ModelAdmin):
 @admin.register(Publisher)
 class PublisherAdmin(admin.ModelAdmin):
     list_display = ("name", "address")
-
+    search_fields = ("name__startswith", )
+    
 @admin.register(Subject)
 class SubjectAdmin(admin.ModelAdmin):
     list_display = ("name",)
+    search_fields = ("name__startswith", )
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ("name", "author", "isbn", "same_author")
     list_filter = ("author",)
+    search_fields = ("name__startswith", )
+    
     def same_author(self, obj):
        
         result = Author.objects.filter(name=obj.author).aggregate(Count("name"))
